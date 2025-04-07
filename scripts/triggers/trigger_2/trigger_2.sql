09e0f967d293d6ca8cc10eb5fa7a5db5d718c957
Chronomortis <113357733+Chronomortis@users.noreply.github.com>
2025-04-07 21:27:00 +0300

    Update trigger_2.sql

--- scripts/triggers/trigger_2/trigger_2.sql
+++ scripts/triggers/trigger_2/trigger_2.sql
@@ -1,1 +1,31 @@
+-- Unsure about its necessity: USE hotel_reservation_db;
 
+-- Trigger(Parking Payment)
+DELIMITER //
+CREATE TRIGGER check_parking_payment 
+BEFORE UPDATE ON Rooms
+FOR EACH ROW
+BEGIN
+DECLARE parking_spot INT;
+DECLARE parking_payment BOOL;
+-- Do only if the vacancy is changed and other aspects are the same.
+IF OLD.Vacancy <> NEW.Vacancy AND NEW.Vacancy = TRUE
+AND OLD.RoomID = NEW.RoomID
+AND OLD.Amenities = NEW.Amenities
+AND OLD.Capacity = NEW.Capacity
+AND OLD.RoomType = NEW.RoomType
+AND OLD.Pricing = NEW.Pricing THEN
+	
+    SELECT U.PSpotNum INTO parking_spot
+	FROM P_Use U, Reserve R
+	WHERE U.GuestID = R.GuestID AND R.RoomID = NEW.RoomID;
+    
+	SELECT PayStatus INTO parking_payment FROM Parking WHERE PSpotNum = parking_spot;
+    IF parking_payment = FALSE THEN
+		SIGNAL SQLSTATE '45000'
+        SET MESSAGE_TEXT = "Parking payment hasn't been completed, checkout request denied.";
+		END IF;
+    END IF;
+END //
+
+DELIMITER ;
